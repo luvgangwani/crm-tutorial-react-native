99977fe07c96635a981618c30c2e09c5a7a5d535
Luv Gangwani <luv.gangwani@gmail.com>
2024-04-01 12:32:38 +1100

    Update the contacts state.

--- src/redux/contacts.ts
+++ src/redux/contacts.ts
@@ -3,11 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 export const contactsSlice = createSlice({
     name: 'contacts',
     initialState: {
-        all: []
+        people: [],
+        detailView: false,
+        personSelected: null,
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        company: '',
+        project: '',
+        notes: '',
+        toUpdate: false,
     },
     reducers: {
+        init: (state, _action) => {
+            fetch('http://localhost:3000/contacts')
+            .then(response => response.json())
+            .then(people => {
+                state.people = people
+            })
+            .catch(error => console.error(error))
+        },
         update: (state, action) => {
-            state.all = action.payload
+            state.people = action.payload
         }
     }
 })
